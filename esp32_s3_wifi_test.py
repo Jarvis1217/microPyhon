2be0dd8083001ee17ab4351cfd45ece18d383931
Jarvis1217 <45352073+Jarvis1217@users.noreply.github.com>
2024-06-05 15:34:05 +0800

    Add files via upload

---
+++ esp32_s3_wifi_test.py
@@ -0,0 +1,36 @@
+import network
+import time
+
+# WiFi网络名称和密码
+target_ssid = 'makerbaby'
+password = 'makerbaby2017'
+
+# 初始化WiFi模块
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+# 扫描WiFi网络
+print("Scanning for WiFi networks...")
+networks = wlan.scan()
+
+# 打印扫描结果
+print("WiFi networks found:")
+for network in networks:
+    ssid = network[0].decode('utf-8')  # 网络名称 (SSID)
+    rssi = network[3]  # 信号强度 (RSSI)
+    print(f"SSID: {ssid}, RSSI: {rssi}")
+
+# 连接到WiFi网络
+print(f'Connecting to {target_ssid}...')
+wlan.connect(target_ssid, password)
+
+# 等待连接成功
+while not wlan.isconnected():
+    print(f'Waiting for connection...{target_ssid}')
+    time.sleep(1)
+
+print('Connected to WiFi')
+print('Network config:', wlan.ifconfig())
+
+# 关闭WiFi模块
+wlan.active(False)
